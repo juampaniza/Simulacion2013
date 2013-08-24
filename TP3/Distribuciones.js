0625e70a79532fdb73fa36f2d850233e7149a2ef
Juampa <juampa.niza@gmail.com>
2013-08-23 21:28:34 -0300

    added box muller

--- TP3/Distribuciones.js
+++ TP3/Distribuciones.js
@@ -19,14 +19,30 @@ function exponencialMedia(media)
 	var result=-media*log;
 	return result;
 }
-function normalConvolucion(mu,g)
+function normalConvolucion(mu, g)
 {
 	var rnd=0;
 	var segundoTermino=0;
 	for (var i = 0; i < 11; i++) {
 		rnd=Math.random();
 		segundoTermino+=rnd-g;
-	};
+	}
 	var result=mu+segundoTermino*g;
+	return result;
+} 
+function normalBoxMuller(mu, g)
+{
+	var rnd1,rnd2;
+	rnd1=Math.random();
+	rnd2=Math.random();
+	var x1,x2;
+	x1=Math.squr(-2*(Math.log(rnd1)))*Math.cos(rnd2);
+	x2=Math.squr(-2*(Math.log(rnd1)))*Math.sin(rnd2);
+	var z1,z2;
+	z1=mu+x1*g;
+	z2=mu+x2*g;
+	var result=[];
+	result.push(z1);
+	result.push(z2);
 	return result;
 }
